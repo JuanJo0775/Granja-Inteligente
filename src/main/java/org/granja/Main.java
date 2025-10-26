e6d3d140b2bcde0e9d17672f9dc720bdfc243db5
JuanJo0775 <jjnaranjo_38@cue.ecu.co>
2025-10-26 12:58:08 -0500

    Pequeños cambios y organización del proyecto

--- src/main/java/org/granja/Main.java
+++ src/main/java/org/granja/Main.java
@@ -1,7 +1,16 @@
 package org.granja;
 
+<<<<<<< Updated upstream
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+=======
+import org.granja.comportamentales.DemoPatronesComportamentales;
+
+/**
+ * Clase principal del proyecto Granja Inteligente
+ * Punto de entrada de la aplicación
+ */
+>>>>>>> Stashed changes
 public class Main {
     public static void main(String[] args) {
 
